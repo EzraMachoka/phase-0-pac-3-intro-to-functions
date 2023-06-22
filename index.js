1afcb44d91ffb3e613a31543f663745a6dd1ef9b
EzraMachoka <ezra.machoka@student.moringaschool.com>
2023-06-22 09:11:47 +0300

    JS Functions

--- index.js
+++ index.js
@@ -1,1 +1,77 @@
 // Follow along with the examples here
+
+function doNothing() {}
+
+
+function sayHello () {
+   console.log("Hello!");
+
+}
+
+sayHello();
+
+
+
+
+function sayHelloToIsabel() {
+    console.log("Hello, Isabel!");
+  }
+
+  function sayHelloToSofia() {
+    console.log("Hello, Sofia!");
+  }
+
+
+  function sayHelloToBrendan() {
+    console.log("Hello, Brendan!");
+  }
+
+  sayHelloToBrendan();
+  sayHelloToIsabel();
+  sayHelloToSofia();
+
+
+
+
+
+
+  function doSomething(thing) {
+    console.log(thing);
+  }
+  
+  doSomething("anything"); 
+
+
+
+
+
+
+
+  function sayHelloTo(firstName) {
+    console.log(`Hello, ${firstName}!`);
+  }
+  
+  sayHelloTo("Isabel"); // "Hello, Isabel!"
+  sayHelloTo("Jane"); // "Hello, Jane!"
+  sayHelloTo("R2-D2"); // "Hello, R2-D2!"
+  sayHelloTo(1);
+
+
+
+
+  function say(greeting, firstName) {
+    console.log(`${greeting}, ${firstName}!`);
+  }
+
+  say("Goodbye", "Julio");
+
+  
+
+
+
+
+  function say(greeting, firstName) {
+    console.log("firstName: ", firstName);
+    console.log("greeting: ", greeting);
+    console.log(`${greeting}, ${firstName}!`);
+  }
